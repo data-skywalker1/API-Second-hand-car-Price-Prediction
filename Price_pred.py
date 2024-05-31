5710e3cd61942d61d2409216683acc0ea3082b4e
Anup  Naktode <144556381+data-skywalker1@users.noreply.github.com>
2024-05-31 16:30:42 +0530

    Update Price_pred.py

--- Price_pred.py
+++ Price_pred.py
@@ -3,90 +3,53 @@ import streamlit as st
 import datetime
 import pickle
 
-# Load the dataset and model
-# since the data is in static file we are we loading like below, however in actual data will come from db or real time data
-cars_df = pd.read_excel("cars24-car-price.xlsx")
-with open("car_pred.pkl", "rb") as file:
-    reg_model = pickle.load(file)
+# right now for the specific use case data is present as a static df, howver in actual sense it wil come from a db
+cars_df=pd.read_excel("cars24-car-price.xlsx")
+
+with open("car_pred.pkl","rb") as file:
+    reg_model=pickle.load(file)
 
 # the title and description
-st.set_page_config(page_title="Car Price Predictor", page_icon="🚗", layout="wide")
+st.set_page_config(page_title="Car Price Predictor", page_icon="🚗")
 
-# image related to second-hand cars
-#st.image("car.jpg", use_column_width=True)
 
-# logo at the top
-#st.image("logo.png", width=100)  # Ensure the correct path to your logo file
-
-st.title("Car Price Predictor")
+st.title("Used Cars Price Prediction")
 st.markdown("### Get the best estimate for your used car!")
 
-# Encoding dictionary
+
 encode_dict = {
     "fuel_type": {'Diesel': 1, 'Petrol': 2, 'CNG': 3, 'LPG': 4, 'Electric': 5},
     "seller_type": {'Dealer': 1, 'Individual': 2, 'Trustmark Dealer': 3},
     "transmission_type": {'Manual': 1, 'Automatic': 2}
 }
 
-def model_pred(fuel_type, transmission_type, engine, seats, reg_model):
-    # here I am taking some features in hard coded manner, we can take them from user also.
-    input_features = [[2018, 1, 4000, fuel_type, transmission_type, 19.7, engine, 86.3, seats]]
-    return reg_model.predict(input_features)
-
-# Layout
-st.markdown("#### Please provide the following details:")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    fuel_type = st.selectbox("Fuel Type", ["Diesel", "Petrol", "CNG", "LPG", "Electric"])
-    engine = st.slider("Engine Power (cc)", 500, 5000, step=100)
-    seats = st.selectbox("Number of Seats", [4, 5, 7, 9, 11])
-
-with col2:
-    transmission_type = st.selectbox("Transmission Type", ["Manual", "Automatic"])
-
-# Prediction button
-if st.button("Predict Price"):
-    fuel_type_encoded = encode_dict['fuel_type'][fuel_type]
-    transmission_type_encoded = encode_dict["transmission_type"][transmission_type]
-    price = model_pred(fuel_type_encoded, transmission_type_encoded, engine, seats, reg_model)
+def model_pred(fuel_type,transmission_type,engine,seats,reg_model):
     
-    st.success(f"🔮 Predicted Price of the car is: ₹{price[0]:,.2f} lakhs")
+        # here I am taking some features in coded manner but we can take them from user
+        input_features=[[2019,1,4000,fuel_type,transmission_type,19.7,engine,86.3,seats]]
 
-# Footer
+        return(reg_model.predict(input_features))
+    
+col1, col2,col3= st.columns(3)
+
+fuel_type=col1.selectbox("Select the fuel type",["Diesel","Petrol","CNG","LPG","Electric"])
+engine=col1.slider("Set the engine power",500,5000,step=100)
+
+transmission_type=col3.selectbox("Select the transmission type",["Manual","Automatic"])
+
+seats=col1.selectbox("Enter the number of seats",[4,5,7,9,11])
+random=col3.slider("Set the random power",500,5000,step=100)
+# random2=col3.selectbox("Enter random2 seats",[4,5,7,9,11])
+
+if (st.button("Predict Price")):
+    fuel_type=encode_dict['fuel_type'][fuel_type]
+    transmission_type=encode_dict["transmission_type"][transmission_type]
+    price=model_pred(fuel_type,transmission_type,engine,seats,reg_model)
+    st.text("Predicted Price of the car is:"+ str(price))
+
+
+
+    # Footer
 st.markdown("---")
 st.markdown("**Note:** This is a simulated prediction based on the given input features.")
 st.markdown("Made with ❤️ by Anup")
-
-# Add some styling
-st.markdown("""
-    <style>
-        body {
-            background-color: white; 
-            color: white
-        }
-        .stButton>button {
-            background-color: #4CAF50;
-            color: white;
-            border: none;
-            padding: 10px 20px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 16px;
-        }
-        .stMarkdown, .stImage {
-            text-align: center;
-        }
-        .stTitle {
-            color: #4CAF50;
-        }
-        .stSelectbox, .stSlider, .stButton {
-            color: #000000;
-        }
-    </style>
-    """, unsafe_allow_html=True)
